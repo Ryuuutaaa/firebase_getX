97ff5bba98d2965592a21c8a59d0690f1c3667d1
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-06-10 11:50:26 +0800

    feat: enhance intro screen with navigation and layout improvements

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get_core/get_core.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 class Intro extends StatelessWidget {
@@ -8,14 +9,30 @@ class Intro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IntroductionScreen(
+      globalBackgroundColor: Colors.white,
       showNextButton: true,
       showDoneButton: true,
-      showSkipButton: true,
+      showSkipButton: false,
+      done: const Text("Done"),
+      onDone: () => Get.to(() => Home()),
+      next: const Icon(Icons.arrow_forward),
       pages: [
         PageViewModel(
           image: SvgPicture.asset('assets/intro1.svg'),
           title: "Todo App",
-        )
+          body: "A simple todo app with a lorem 12 words and a button",
+          decoration: const PageDecoration(
+            imagePadding: EdgeInsets.only(top: 50.0),
+          ),
+        ),
+        PageViewModel(
+          image: SvgPicture.asset('assets/intro2.svg'),
+          title: "Easy to use",
+          body: "A simple todo app with a lorem 12 words and a button",
+          decoration: const PageDecoration(
+            imagePadding: EdgeInsets.only(top: 50.0),
+          ),
+        ),
       ],
     );
   }
